e00e938ca90a0600869a6dd24b4dd44398a99a84
Mikkosar <Mikkosar12@gmail.com>
2024-03-17 10:37:37 +0200

    H2-console security

--- src/main/java/hh/sof3/bookstore/WebSecurityConfig.java
+++ src/main/java/hh/sof3/bookstore/WebSecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
@@ -33,7 +34,15 @@ public class WebSecurityConfig {
         http
         .authorizeHttpRequests( authorize -> authorize
         .requestMatchers(antMatcher("/css/**")).permitAll()
+        .requestMatchers(toH2Console()).permitAll()
             .anyRequest().authenticated()
+        )
+        .csrf(csrf -> csrf
+        .ignoringRequestMatchers(toH2Console())
+        )
+        .headers(headers -> headers
+        .frameOptions(frameoptions -> frameoptions
+            .disable())
         )
         .formLogin(formlogin -> formlogin
             .loginPage("/login")
